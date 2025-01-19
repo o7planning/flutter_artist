b9155d05594c095f4056de731c77552bdc666a46
o7planning <10128477+o7planning@users.noreply.github.com>
2025-01-19 19:36:09 +0600

    .

--- lib/_core_/_suggested_form_data.dart
+++ lib/_core_/_suggested_form_data.dart
@@ -1,5 +1,13 @@
 part of '../flutter_artist.dart';
 
+abstract class ExtraFormInput {
+  //
+}
+
+class EmptyExtraFormData extends ExtraFormInput {
+  //
+}
+
 abstract class SuggestedFormData {
   FormAction formAction = FormAction.create;
 
