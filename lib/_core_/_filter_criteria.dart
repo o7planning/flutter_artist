fcb75298a81a65fd73f80daa925bc7596cd26640
o7planning <10128477+o7planning@users.noreply.github.com>
2025-01-20 09:27:49 +0600

    .

--- lib/_core_/_filter_criteria.dart
+++ lib/_core_/_filter_criteria.dart
@@ -1,10 +1,14 @@
 part of '../flutter_artist.dart';
 
+@immutable
 abstract class FilterCriteria extends Equatable {
   const FilterCriteria();
 }
 
+@immutable
 class EmptyFilterCriteria extends FilterCriteria {
+  const EmptyFilterCriteria();
+
   @override
   List<Object?> get props => [];
 }
