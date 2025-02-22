fc4059d6a9cab279b7d25784d7d74a44a804a5f6
o7planning <10128477+o7planning@users.noreply.github.com>
2025-02-22 14:28:07 +0600

    .

--- lib/_coordinator_/_coordinator.dart
+++ lib/_coordinator_/_coordinator.dart
@@ -1,5 +1,35 @@
 part of '../flutter_artist.dart';
 
 abstract class Coordinator extends _XBase {
-  Future<void> coordinate();
+  void Function()? customNavigate;
+
+  Coordinator({required this.customNavigate});
+
+  Future<bool> execute() async {
+    try {
+      bool success = await coordinationLogic();
+      if (!success) {
+        return false;
+      }
+      if (customNavigate != null) {
+        customNavigate!();
+      } else {
+        await defaultNavigate();
+      }
+      return true;
+    } catch (e, stackTrace) {
+      showErrorSnackBar(
+        message: "${getClassName(this)} execute error",
+        errorDetails: ["Error: $e"],
+      );
+      return false;
+    }
+  }
+
+  ///
+  /// Do not call this method. Call execute() instead.
+  ///
+  Future<bool> coordinationLogic();
+
+  Future<void> defaultNavigate();
 }
