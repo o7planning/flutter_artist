6ee7edfde63457c8c8223ecc06b367cde8f10d4e
o7planning <10128477+o7planning@users.noreply.github.com>
2025-03-01 14:38:35 +0600

    .

---
+++ lib/_enum_/_task_unit_result.dart
@@ -0,0 +1,9 @@
+part of '../flutter_artist.dart';
+
+enum TaskUnitResult {
+  cancelled,
+  successIgnore,
+  success,
+  failIgnore,
+  fail;
+}
--- lib/flutter_artist.dart
+++ lib/flutter_artist.dart
@@ -285,6 +285,8 @@ part '_task_unit_/_block_quick_action_task_unit.dart';
 
 part '_task_unit_/_block_select_as_current_task_unit.dart';
 
+part '_enum_/_task_unit_result.dart';
+
 part '_task_unit_/_block_query_task_unit.dart';
 
 part '_task_unit_/_block_quick_child_block_items_task_unit.dart';
