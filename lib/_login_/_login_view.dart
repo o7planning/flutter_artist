64e12e496241ca5c53cd4db08849221f6b9d836e
o7planning <10128477+o7planning@users.noreply.github.com>
2025-04-10 13:59:46 +0600

    .

--- lib/_login_/_login_view.dart
+++ lib/_login_/_login_view.dart
@@ -1,17 +1,24 @@
 part of '../flutter_artist.dart';
 
 class LoginView extends StatelessWidget {
+  final LoginZilch loginZilch;
   final LoginModel loginModel;
 
   const LoginView({
     super.key,
+    required this.loginZilch,
     required this.loginModel,
   });
 
   @override
   Widget build(BuildContext context) {
-    // return VoidFragmentWidgetBuilder(ownerClassInstance: this,
-    //     description: null);
-    throw UnimplementedError();
+    return ZilchFragmentWidgetBuilder(
+      zilch: loginZilch,
+      ownerClassInstance: this,
+      description: null,
+      build: () {
+        return Text("OK");
+      },
+    );
   }
 }
