15e904c23ea858d704446caa74e768e7255c17ce
o7planning <10128477+o7planning@users.noreply.github.com>
2025-05-20 00:16:07 +0600

    .

--- lib/src/_core_state_/_force_reload_state.dart
+++ lib/src/_core_state_/_force_reload_state.dart
@@ -17,25 +17,93 @@ class ForceReloadDebug {
 
 final Map<String, ForceReloadDebug> forceReloadMap = {};
 
+void initDebugForceReloadMap() {
+  forceReloadMap["ITM 1.1.1.1.1"] = ForceReloadDebug("");
+  forceReloadMap["ITM 1.1.1.1.2"] = ForceReloadDebug("");
+  forceReloadMap["ITM 1.1.1.2.1"] = ForceReloadDebug("");
+  forceReloadMap["ITM 1.1.1.2.2"] = ForceReloadDebug("");
+  forceReloadMap["ITM 1.1.2.1.1"] = ForceReloadDebug("");
+  forceReloadMap["ITM 1.1.2.1.2"] = ForceReloadDebug("");
+  forceReloadMap["ITM 1.1.2.2.1"] = ForceReloadDebug("");
+  forceReloadMap["ITM 1.1.2.2.2"] = ForceReloadDebug("");
+  forceReloadMap["ITM 1.2.1.1.1"] = ForceReloadDebug("");
+  forceReloadMap["ITM 1.2.1.1.2"] = ForceReloadDebug("");
+  forceReloadMap["ITM 1.2.1.2.1"] = ForceReloadDebug("");
+  forceReloadMap["ITM 1.2.1.2.2"] = ForceReloadDebug("");
+  forceReloadMap["ITM 1.2.2.1.1"] = ForceReloadDebug("");
+  forceReloadMap["ITM 1.2.2.1.2"] = ForceReloadDebug("");
+  forceReloadMap["ITM 1.2.2.2.1"] = ForceReloadDebug("");
+  forceReloadMap["ITM 1.2.2.2.2"] = ForceReloadDebug("");
+  forceReloadMap["ITM 2.1.1.1.1"] = ForceReloadDebug("");
+  forceReloadMap["ITM 2.1.1.1.2"] = ForceReloadDebug("");
+  forceReloadMap["ITM 2.1.1.2.1"] = ForceReloadDebug("");
+  forceReloadMap["ITM 2.1.1.2.2"] = ForceReloadDebug("");
+  forceReloadMap["ITM 2.1.2.1.1"] = ForceReloadDebug("");
+  forceReloadMap["ITM 2.1.2.1.2"] = ForceReloadDebug("");
+  forceReloadMap["ITM 2.1.2.2.1"] = ForceReloadDebug("");
+  forceReloadMap["ITM 2.1.2.2.2"] = ForceReloadDebug("");
+  forceReloadMap["ITM 2.2.1.1.1"] = ForceReloadDebug("");
+  forceReloadMap["ITM 2.2.1.1.2"] = ForceReloadDebug("");
+  forceReloadMap["ITM 2.2.1.2.1"] = ForceReloadDebug("");
+  forceReloadMap["ITM 2.2.1.2.2"] = ForceReloadDebug("");
+  forceReloadMap["ITM 2.2.2.1.1"] = ForceReloadDebug("");
+  forceReloadMap["ITM 2.2.2.1.2"] = ForceReloadDebug("");
+  forceReloadMap["ITM 2.2.2.2.1"] = ForceReloadDebug("");
+  forceReloadMap["ITM 2.2.2.2.2"] = ForceReloadDebug("");
+  forceReloadMap["ITM 3.1.1.1.1"] = ForceReloadDebug("");
+  forceReloadMap["ITM 3.1.1.1.2"] = ForceReloadDebug("");
+  forceReloadMap["ITM 3.1.1.2.1"] = ForceReloadDebug("");
+  forceReloadMap["ITM 3.1.1.2.2"] = ForceReloadDebug("");
+  forceReloadMap["ITM 3.1.2.1.1"] = ForceReloadDebug("");
+  forceReloadMap["ITM 3.1.2.1.2"] = ForceReloadDebug("");
+  forceReloadMap["ITM 3.1.2.2.1"] = ForceReloadDebug("");
+  forceReloadMap["ITM 3.1.2.2.2"] = ForceReloadDebug("");
+  forceReloadMap["ITM 3.2.1.1.1"] = ForceReloadDebug("");
+  forceReloadMap["ITM 3.2.1.1.2"] = ForceReloadDebug("");
+  forceReloadMap["ITM 3.2.1.2.1"] = ForceReloadDebug("");
+  forceReloadMap["ITM 3.2.1.2.2"] = ForceReloadDebug("");
+  forceReloadMap["ITM 3.2.2.1.1"] = ForceReloadDebug("");
+  forceReloadMap["ITM 3.2.2.1.2"] = ForceReloadDebug("");
+  forceReloadMap["ITM 3.2.2.2.1"] = ForceReloadDebug("");
+  forceReloadMap["ITM 3.2.2.2.2"] = ForceReloadDebug("");
+}
+
 void printDebugForceReloadMap() {
   print(" **************************************************************** ");
   print(" **************************************************************** ");
   // key: 20a, 43a,..
+  int i = 1;
   for (String key in forceReloadMap.keys) {
     ForceReloadDebug debug = forceReloadMap[key]!;
     String newTestCodes = debug.toTestCodes();
     bool changed = debug.testCodes != newTestCodes;
-    if (!changed) {
-      print("KEY: $key - $newTestCodes");
+    if (!changed && newTestCodes.isNotEmpty) {
+      print("${i.toString().padLeft(2, '0')} KEY: $key - $newTestCodes");
+      i++;
     }
   }
   //
+  print("\n");
+  int j = 1;
   for (String key in forceReloadMap.keys) {
     ForceReloadDebug debug = forceReloadMap[key]!;
     String newTestCodes = debug.toTestCodes();
     bool changed = debug.testCodes != newTestCodes;
-    if (changed) {
-      print("KEY: $key - $newTestCodes ${changed ? '***' : ''}");
+    if (changed && newTestCodes.isNotEmpty) {
+      print(
+          "${j.toString().padLeft(2, '0')} KEY: $key - $newTestCodes ${changed ? '***' : ''}");
+      j++;
+    }
+  }
+  //
+  print("\n");
+  int k = 1;
+  for (String key in forceReloadMap.keys) {
+    ForceReloadDebug debug = forceReloadMap[key]!;
+    String newTestCodes = debug.toTestCodes();
+    if (newTestCodes.isNotEmpty) {
+      print("${k.toString().padLeft(2, '0')} EMPTY: $key");
+      k++;
     }
   }
   print(" **************************************************************** ");
@@ -62,9 +130,10 @@ void _addDebugForceReload({
   if (no != null) {
     ForceReloadDebug? debug = forceReloadMap[debugCode];
     if (debug == null) {
-      debug = ForceReloadDebug(testCodes);
-      forceReloadMap[debugCode] = debug;
+      return;
     }
+    // testCodes = "20a, 30b".
+    debug.testCodes = testCodes;
     debug.addTestCode(testCode);
   }
 }
