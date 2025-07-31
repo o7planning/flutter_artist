ffffb41a494c31375b4880f7b5dafefedbb38d96
o7planning <10128477+o7planning@users.noreply.github.com>
2025-07-31 15:31:14 +0600

    .

--- lib/src/core/action/base_async_action.dart
+++ lib/src/core/action/base_async_action.dart
@@ -3,8 +3,8 @@ import 'package:flutter_artist_core/flutter_artist_core.dart';
 import '../enums/after_quick_action.dart';
 import 'base_action.dart';
 
-abstract class AsyncBaseAction extends BaseAction {
-  AsyncBaseAction({
+abstract class BaseAsyncAction extends BaseAction {
+  BaseAsyncAction({
     required super.needToConfirm,
     required super.actionInfo,
   });
--- lib/src/core/action/download_quick_action.dart
+++ lib/src/core/action/download_quick_action.dart
@@ -1,14 +1,12 @@
 import 'package:flutter_artist_core/flutter_artist_core.dart';
 
 import '../enums/after_quick_action.dart';
-import 'async_base_quick_action.dart';
+import 'base_async_action.dart';
 import 'base_action.dart';
 
-abstract class DownloadQuickAction extends AsyncBaseAction {
+abstract class DownloadQuickAction extends BaseAsyncAction {
   DownloadQuickAction({
     required super.needToConfirm,
     required super.actionInfo,
   });
-
-  Future<ApiResult<void>> callApi();
 }
