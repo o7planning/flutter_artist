893ab4eb7685132ee5fb1038fe4f381b23e164b0
o7planning <10128477+o7planning@users.noreply.github.com>
2025-08-02 13:09:22 +0600

    .

--- lib/src/core/precheck/block_multi_items_creation_precheck.dart
+++ lib/src/core/precheck/block_multi_items_creation_precheck.dart
@@ -4,25 +4,25 @@ import '__chk_code_detail.dart';
 enum BlockMultiItemsCreationPrecheck implements ChkCodeDetail {
   busy(
     chkCode: ChkCode.busy,
-    message: "New item creation is disabled.",
+    message: "Multi Items creation is disabled.",
     details: ["The executor is busy."],
   ),
   //
   blockInPendingState(
     chkCode: ChkCode.inPendingState,
-    message: "New item creation is disabled.",
+    message: "Multi Items creation is disabled.",
     details: ["The block is in a 'pending' state."],
   ),
   //
   blockInErrorState(
     chkCode: ChkCode.inErrorState,
-    message: "New item creation is disabled.",
+    message: "Multi Items creation is disabled.",
     details: ["The block is in an 'error' state."],
   ),
   //
   blockInNoneState(
     chkCode: ChkCode.inNoneState,
-    message: "New item creation is disabled.",
+    message: "Multi Items creation is disabled.",
     details: ["The block is in a 'none' state."],
   ),
   //
