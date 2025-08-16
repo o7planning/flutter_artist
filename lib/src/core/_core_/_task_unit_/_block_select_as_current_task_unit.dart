33092175e123ee089521a88717dc02bc849e759f
o7planning <10128477+o7planning@users.noreply.github.com>
2025-08-17 03:15:09 +0600

    .

--- lib/src/core/_core_/_task_unit_/_block_select_as_current_task_unit.dart
+++ lib/src/core/_core_/_task_unit_/_block_select_as_current_task_unit.dart
@@ -6,63 +6,6 @@ part of '../core.dart';
 @_BlockRefreshAndSelectNextItemAsCurrentAnnotation()
 @_BlockRefreshAndSelectFirstItemAsCurrentAnnotation()
 @_BlockRefreshAndSelectPreviousItemAsCurrentAnnotation()
-@Deprecated("Xoa di")
-class _BlockSelectAsCurrentTaskUnitOLD<ITEM extends Object>
-    extends _ResultedTaskUnit<BlockItemCurrSelectionResult<ITEM>> {
-  final _XBlock xBlock;
-  final ForceType? forceTypeForForm;
-  final CurrentItemSelectionType currentItemSelectionType;
-  final ITEM? candidateItem;
-  final List<ITEM> newQueriedList;
-
-  _BlockSelectAsCurrentTaskUnitOLD({
-    required this.currentItemSelectionType,
-    required this.xBlock,
-    required this.newQueriedList,
-    required this.candidateItem,
-    required bool forceReloadItem,
-    required this.forceTypeForForm,
-  }) : super(
-          taskType: TaskType.blockSelectItemAsCurrent,
-          taskResult: BlockItemCurrSelectionResult<ITEM>(
-            precheck: null,
-            currentItemSelectionType: currentItemSelectionType,
-            getItemId: xBlock.block.getItemId,
-            candidateItem: candidateItem,
-            oldCurrentItem: xBlock.block.currentItem as ITEM?,
-            currentItem: xBlock.block.currentItem as ITEM?,
-          ),
-        ) {
-    if (forceReloadItem) {
-      xBlock.setForceReloadItem();
-    }
-    //
-    if (forceTypeForForm != null) {
-      xBlock.xFormModel?.forceTypeForForm = forceTypeForForm!;
-    }
-  }
-
-  @override
-  int get xShelfId => xBlock.xShelfId;
-
-  @override
-  Shelf get shelf => xBlock.block.shelf;
-
-  @override
-  Block get owner => xBlock.block;
-
-  @override
-  String getObjectName() {
-    return xBlock.block.name;
-  }
-}
-
-@_TaskUnitClassAnnotation()
-@_BlockRefreshCurrentItemAnnotation()
-@_BlockSelectItemAsCurrentAnnotation()
-@_BlockRefreshAndSelectNextItemAsCurrentAnnotation()
-@_BlockRefreshAndSelectFirstItemAsCurrentAnnotation()
-@_BlockRefreshAndSelectPreviousItemAsCurrentAnnotation()
 class _BlockSelectAsCurrentTaskUnit<ITEM extends Object>
     extends _ResultedTaskUnit<BlockItemCurrSelectionResult<ITEM>> {
   final _QBlock xBlock;
