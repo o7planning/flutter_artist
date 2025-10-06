165d1337f789ff6154dbd7b7c3adb6f915890475
o7planning <10128477+o7planning@users.noreply.github.com>
2025-10-06 15:30:29 +0600

    ... begin 1.2.2 ...

--- lib/src/core/_core_/_observer_/_navigator_observer.dart
+++ lib/src/core/_core_/_observer_/_navigator_observer.dart
@@ -1,8 +1,10 @@
 part of '../core.dart';
 
-class _FlutterArtistNavigatorObserver extends NavigatorObserver {
+// https://pub.dev/packages/navigation_history_observer/example
+class _FlutterArtistNavigatorObserver extends RouteObserver<PageRoute> {
   @override
   void didPush(Route route, Route? previousRoute) {
+    super.didPush(route, previousRoute);
     DebugPrinter.printDebug(
       DebugCat.navigatorObserver,
       '[NavigatorObserver] -----------------> Route didPush: ${route.settings.name} - previousRoute: ${previousRoute?.settings.name}',
@@ -10,7 +12,17 @@ class _FlutterArtistNavigatorObserver extends NavigatorObserver {
   }
 
   @override
+  bool debugObservingRoute(PageRoute route) {
+    DebugPrinter.printDebug(
+      DebugCat.navigatorObserver,
+      '[NavigatorObserver] -----------------> Route debugObservingRoute: ${route.settings.name}',
+    );
+    return super.debugObservingRoute(route);
+  }
+
+  @override
   void didPop(Route route, Route? previousRoute) {
+    super.didPop(route, previousRoute);
     DebugPrinter.printDebug(
       DebugCat.navigatorObserver,
       '[NavigatorObserver] -----------------> Route didPop: ${route.settings.name} - previousRoute: ${previousRoute?.settings.name}',
@@ -19,6 +31,7 @@ class _FlutterArtistNavigatorObserver extends NavigatorObserver {
 
   @override
   void didChangeTop(Route<dynamic> topRoute, Route<dynamic>? previousTopRoute) {
+    super.didChangeTop(topRoute, previousTopRoute);
     DebugPrinter.printDebug(
       DebugCat.navigatorObserver,
       '[NavigatorObserver] -----------------> Route didChangeTop: ${topRoute.settings.name} - previousTopRoute: ${previousTopRoute?.settings.name}',
@@ -27,6 +40,7 @@ class _FlutterArtistNavigatorObserver extends NavigatorObserver {
 
   @override
   void didRemove(Route<dynamic> route, Route<dynamic>? previousRoute) {
+    super.didRemove(route, previousRoute);
     DebugPrinter.printDebug(
       DebugCat.navigatorObserver,
       '[NavigatorObserver] -----------------> Route didRemove: ${route.settings.name} - previousRoute: ${previousRoute?.settings.name}',
@@ -35,6 +49,8 @@ class _FlutterArtistNavigatorObserver extends NavigatorObserver {
 
   @override
   void didReplace({Route<dynamic>? newRoute, Route<dynamic>? oldRoute}) {
+    super.didReplace(newRoute: newRoute, oldRoute: oldRoute);
+
     DebugPrinter.printDebug(
       DebugCat.navigatorObserver,
       '[NavigatorObserver] -----------------> Route didReplace: ${newRoute?.settings?.name} - oldRoute: ${oldRoute?.settings.name}',
@@ -44,6 +60,8 @@ class _FlutterArtistNavigatorObserver extends NavigatorObserver {
   @override
   void didStartUserGesture(
       Route<dynamic> route, Route<dynamic>? previousRoute) {
+    super.didStartUserGesture(route, previousRoute);
+
     DebugPrinter.printDebug(
       DebugCat.navigatorObserver,
       '[NavigatorObserver] -----------------> Route didStartUserGesture: ${route.settings.name} - previousRoute: ${previousRoute?.settings.name}',
@@ -52,9 +70,28 @@ class _FlutterArtistNavigatorObserver extends NavigatorObserver {
 
   @override
   void didStopUserGesture() {
+    super.didStopUserGesture();
     DebugPrinter.printDebug(
       DebugCat.navigatorObserver,
       '[NavigatorObserver] -----------------> Route didStopUserGesture',
+    );
+  }
+
+  @override
+  void subscribe(RouteAware routeAware, PageRoute route) {
+    super.subscribe(routeAware, route);
+    DebugPrinter.printDebug(
+      DebugCat.navigatorObserver,
+      '[NavigatorObserver] -----------------> Route subscribe: ${route.settings.name} - routeAware: ${routeAware}',
+    );
+  }
+
+  @override
+  void unsubscribe(RouteAware routeAware) {
+    super.unsubscribe(routeAware);
+    DebugPrinter.printDebug(
+      DebugCat.navigatorObserver,
+      '[NavigatorObserver] -----------------> Route unsubscribe: routeAware: ${routeAware}',
     );
   }
 }
--- lib/src/core/_core_/_ui_/__refreshable_widget_state.dart
+++ lib/src/core/_core_/_ui_/__refreshable_widget_state.dart
@@ -12,8 +12,14 @@ interface class IRefreshableWidgetState {
   void setState(Function() func) {}
 }
 
+// Google Search: Flutter NavigatorObserver Aware.
 abstract class _RefreshableWidgetState<W extends _RefreshableWidget>
-    extends State<W> implements IRefreshableWidgetState {
+    extends State<W> //
+    // Google Search: Flutter NavigatorObserver Aware.
+    with
+        RouteAware
+    implements
+        IRefreshableWidgetState {
   double __maxSize = 0;
 
   double get maxSize => __maxSize;
@@ -106,7 +112,7 @@ abstract class _RefreshableWidgetState<W extends _RefreshableWidget>
   void __removeWidgetState() {
     DebugPrinter.printDebug(
       DebugCat.visibilityDetector,
-      "[VisibilityDetector] ------------> Remove ${getClassNameWithoutGenerics(this.widget)}",
+      "[VisibilityDetector] ------------> Remove ${getClassNameWithoutGenerics(widget)}",
     );
     removeWidgetState();
     // FlutterArtist.storage._freezeMan._onWidgetStateDisposed(
@@ -135,9 +141,42 @@ abstract class _RefreshableWidgetState<W extends _RefreshableWidget>
 
   @override
   void dispose() {
+    FlutterArtist.navigatorObserver.unsubscribe(this);
     super.dispose();
     __removeWidgetState();
     //
     checkAndFreeMemory();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    FlutterArtist.navigatorObserver
+        .subscribe(this, ModalRoute.of(context)! as PageRoute);
+  }
+
+  @override
+  void didPush() {
+    // This route has been pushed onto the navigator.
+    // You can infer this is the current route when this method is called.
+    print('  [RouteAware] ---------> didPush: ${ModalRoute.of(context)?.settings.name} - ${getClassNameWithoutGenerics(widget)}');
+  }
+
+  @override
+  void didPopNext() {
+    // Another route was popped off, and this route is now the current one.
+    print('  [RouteAware] ---------> didPopNext: ${ModalRoute.of(context)?.settings.name} - ${getClassNameWithoutGenerics(widget)}');
+  }
+
+  @override
+  void didPushNext() {
+    // A new route has been pushed on top of this one.
+    print('  [RouteAware] ---------> didPushNext: ${ModalRoute.of(context)?.settings.name} - ${getClassNameWithoutGenerics(widget)}');
+  }
+
+  @override
+  void didPop() {
+    // This route has been popped off the navigator.
+    print('  [RouteAware] ---------> didPop: ${ModalRoute.of(context)?.settings.name} - ${getClassNameWithoutGenerics(widget)}');
   }
 }
